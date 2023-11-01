bc19a2fc3f874000f354ac3ab2925ca7fa4cd28e
devsince2021 <dev.ryu2021@gmail.com>
2023-11-01 15:58:15 +0900

    feat: 실종 동물 리스트를 위한 컨트롤러 로직 작성

---
+++ src/main/java/com/project/animal/missing/constant/EndPoint.java
@@ -0,0 +1,6 @@
+package com.project.animal.missing.constant;
+
+public class EndPoint {
+  public static final String MISSING = "/missing";
+  public static final String LIST = "/list";
+}
---
+++ src/main/java/com/project/animal/missing/dummy/MissingPostDummy.java
@@ -0,0 +1,21 @@
+package com.project.animal.missing.dummy;
+
+import com.project.animal.missing.domain.MissingPost;
+import com.project.animal.missing.dto.MissingListResDto;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MissingPostDummy {
+  private static List<MissingPost> dummyPost = List.of(
+          new MissingPost(1, 1,"Lost Friendly Dog", 'd', "Brown", 150, "Central Park", LocalDateTime.now(), "Sma dog with a red collar and a friendly demeanor.", 0, 'L', true),
+          new MissingPost(1, 1,"Lost Friendly Dog", 'd', "Brown", 150, "Central Park", LocalDateTime.now(), "Sma dog with a red collar and a friendly demeanor.", 0, 'L', true),
+          new MissingPost(1, 1,"Lost Friendly Dog", 'd', "Brown", 150, "Central Park", LocalDateTime.now(), "Sma dog with a red collar and a friendly demeanor.", 0, 'L', true)
+  );
+
+  public static List<MissingListResDto> getDummyDto() {
+   return dummyPost.stream().map((entity) -> MissingListResDto.fromMissingPost(entity)).collect(Collectors.toList());
+  }
+}
