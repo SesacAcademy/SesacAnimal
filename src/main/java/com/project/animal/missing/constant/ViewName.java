6139edcce8fdd8855bc04eb974c41c99d5bf4a34
devsince2021 <dev.ryu2021@gmail.com>
2023-11-02 20:14:44 +0900

    chore: 파일 패스 변경

--- src/main/java/com/project/animal/missing/constant/ViewName.java
+++ src/main/java/com/project/animal/missing/constant/ViewName.java
@@ -3,6 +3,6 @@ package com.project.animal.missing.constant;
 public class ViewName {
 
   public static final String PREFIX = "/missing";
-  public static final String POST_LIST = PREFIX + "/missingList";
-  public static final String POST_DETAIL = PREFIX + "/missingDetail";
+  public static final String POST_LIST = PREFIX + "/list/missingList";
+  public static final String POST_DETAIL = PREFIX + "/detail/missingDetail";
 }
