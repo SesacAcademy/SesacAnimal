c051ac22e5050456f49423bd4300d97484400578
dev-lkj <2_kj@daum.net>
2023-11-02 16:39:53 +0900

    feat: 입양 게시판 css 일부 수정

---
+++ src/main/generated/com/project/animal/missing/domain/QMissingPost.java
@@ -0,0 +1,63 @@
+package com.project.animal.missing.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMissingPost is a Querydsl query type for MissingPost
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMissingPost extends EntityPathBase<MissingPost> {
+
+    private static final long serialVersionUID = 1179330516L;
+
+    public static final QMissingPost missingPost = new QMissingPost("missingPost");
+
+    public final ComparablePath<Character> animalType = createComparable("animalType", Character.class);
+
+    public final StringPath color = createString("color");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final StringPath description = createString("description");
+
+    public final BooleanPath isActive = createBoolean("isActive");
+
+    public final NumberPath<Long> memberId = createNumber("memberId", Long.class);
+
+    public final NumberPath<Long> missingId = createNumber("missingId", Long.class);
+
+    public final StringPath missingPlace = createString("missingPlace");
+
+    public final ComparablePath<Character> missingStatus = createComparable("missingStatus", Character.class);
+
+    public final DateTimePath<java.time.LocalDateTime> missingTime = createDateTime("missingTime", java.time.LocalDateTime.class);
+
+    public final NumberPath<Integer> reward = createNumber("reward", Integer.class);
+
+    public final StringPath title = createString("title");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Integer> viewCount = createNumber("viewCount", Integer.class);
+
+    public QMissingPost(String variable) {
+        super(MissingPost.class, forVariable(variable));
+    }
+
+    public QMissingPost(Path<? extends MissingPost> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMissingPost(PathMetadata metadata) {
+        super(MissingPost.class, metadata);
+    }
+
+}
+
