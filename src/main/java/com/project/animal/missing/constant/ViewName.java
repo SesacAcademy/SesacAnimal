dcd110fbddfba36782b12d94a3460f4fc83ad54e
devsince2021 <dev.ryu2021@gmail.com>
2023-11-03 15:07:01 +0900

    chore: 상수 클래스 변경

--- src/main/java/com/project/animal/missing/constant/ViewName.java
+++ src/main/java/com/project/animal/missing/constant/ViewName.java
@@ -1,8 +1,8 @@
 package com.project.animal.missing.constant;
 
-public class ViewName {
-
+abstract public class ViewName {
   public static final String PREFIX = "missing";
   public static final String POST_LIST = PREFIX + "/list/missingList";
   public static final String POST_DETAIL = PREFIX + "/detail/missingDetail";
+  public static final String POST_NEW = PREFIX + "/new/missingNew";
 }
