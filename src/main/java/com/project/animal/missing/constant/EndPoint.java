6bd9a15310c707adb6ac9ca2917257a85fd9a028
devsince2021 <dev.ryu2021@gmail.com>
2023-11-03 14:25:38 +0900

    chore: 상수 변경

--- src/main/java/com/project/animal/missing/constant/EndPoint.java
+++ src/main/java/com/project/animal/missing/constant/EndPoint.java
@@ -5,8 +5,6 @@ public class EndPoint {
   public static final String LIST = "/list";
 
   public static final String POST_ID = "/{postId}";
-  public static final String DETAIL = MISSING + POST_ID;
-
-
+  public static final String DETAIL = POST_ID;
 
 }
