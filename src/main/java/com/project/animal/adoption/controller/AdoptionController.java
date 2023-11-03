64607395a8e47a9301927fc2b8daed064eeb7101
dev-lkj <2_kj@daum.net>
2023-11-03 15:12:27 +0900

    design: 입양 게시판 뷰 댓글 영역 추가, 조회수, 좋아요 추가, 이미지 추가/보기 영역 추가, 취소버튼 추가, 수정버튼 색상 수정

--- src/main/java/com/project/animal/adoption/controller/AdoptionController.java
+++ src/main/java/com/project/animal/adoption/controller/AdoptionController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("v1/adoption")
+@RequestMapping("/v1/adoption")
 public class AdoptionController {
 
 
