00fb5f1ce3545578b0d5698b168ea4f1395bdd7d
sHu <akgkfk9@gmail.com>
2023-11-05 00:01:29 +0900

    feat: Member 엔티티 및 레포지토리 작성

--- src/main/java/com/project/animal/member/domain/Member.java
+++ src/main/java/com/project/animal/member/domain/Member.java
@@ -1,16 +1,47 @@
 package com.project.animal.member.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
-    @Column
+    @Column(length = 100)
     private String email;
 
-    @Column
+    @Column(length = 1000)
     private String password;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String phone;
+
+    @Column(length = 20)
+    private String grade;
+
+    @Column(length = 20)
+    private String type;
+
+    @Column(length = 1)
+    private String status;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime lastLoginAt;
 }
--- src/main/java/com/project/animal/member/repository/MemberRepository.java
+++ src/main/java/com/project/animal/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.project.animal.member.repository;
 import com.project.animal.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
