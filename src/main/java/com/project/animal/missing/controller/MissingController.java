51544b00e62b0b081e371de2170689f469311fcc
devsince2021 <dev.ryu2021@gmail.com>
2023-11-06 13:45:36 +0900

    feat: 목록 호출 관련 dto 및 컨트롤러 작업

--- src/main/java/com/project/animal/missing/controller/MissingController.java
+++ src/main/java/com/project/animal/missing/controller/MissingController.java
@@ -2,15 +2,18 @@ package com.project.animal.missing.controller;
 
 import com.project.animal.missing.constant.EndPoint;
 import com.project.animal.missing.constant.ViewName;
+import com.project.animal.missing.dto.MissingListReqDto;
 import com.project.animal.missing.dto.MissingListResDto;
 import com.project.animal.missing.dummy.MissingPostDummy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.swing.text.View;
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -19,9 +22,10 @@ import java.util.List;
 public class MissingController {
 
   @GetMapping(EndPoint.LIST)
-  public String getPostList(Model model) {
+  public String getPostList(@Valid MissingListReqDto dto, BindingResult br, Model model) {
     List<MissingListResDto> list = MissingPostDummy.getDummyDto();
     model.addAttribute("list", list);
+    model.addAttribute("count", 100);
 
     return ViewName.POST_LIST;
   }
@@ -35,7 +39,6 @@ public class MissingController {
     model.addAttribute("comments", comments);
 
     return ViewName.POST_DETAIL;
-
   }
 
   @GetMapping(EndPoint.NEW)
