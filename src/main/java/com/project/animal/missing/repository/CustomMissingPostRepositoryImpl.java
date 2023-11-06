52853d78647fa56e4d6da3ca0acfa5b9e30a1d5a
devsince2021 <dev.ryu2021@gmail.com>
2023-11-07 01:35:17 +0900

    feat: 실종게시판 repository 구현

---
+++ src/main/java/com/project/animal/missing/repository/CustomMissingPostRepositoryImpl.java
@@ -0,0 +1,70 @@
+package com.project.animal.missing.repository;
+
+import com.project.animal.missing.domain.MissingPost;
+import com.project.animal.missing.dto.MissingFilterDto;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import com.project.animal.missing.domain.QMissingPost;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Slf4j
+public class CustomMissingPostRepositoryImpl implements CustomMissingPostRepository {
+
+  private final int isActive = 1;
+  private final JPAQueryFactory queryFactory;
+
+  @Autowired
+  public CustomMissingPostRepositoryImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
+
+  @Override
+  public Page<MissingPost> findByFilter(MissingFilterDto filter, Pageable pageable) {
+    QMissingPost qMissing = QMissingPost.missingPost;
+
+    BooleanBuilder builder = new BooleanBuilder();
+
+    builder.and(qMissing.isActive.eq(isActive));
+
+    if (filter.getAnimalType() != null) {
+      builder.and(qMissing.animalType.equalsIgnoreCase(filter.getAnimalType()));
+    }
+
+    // TODO: fromDate로 조건 검색시 일치하지 않음
+    if (filter.getFromDate() != null) {
+      builder.and(qMissing.missingTime.goe(filter.getFromDate()));
+    }
+
+    if (filter.getEndDate() != null) {
+      builder.and(qMissing.missingTime.loe(filter.getEndDate()));
+    }
+
+    // NOTE: 검색은 타이틀 기준으로 실행
+    if (filter.getSearch() != null && !filter.getSearch().isBlank() && !filter.getSearch().isEmpty()) {
+      builder.and(qMissing.title.containsIgnoreCase(filter.getSearch()));
+    }
+
+    List<MissingPost> results = queryFactory
+            .selectFrom(qMissing)
+            .where(builder)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy()
+            .fetch();
+
+    long total = queryFactory
+            .select(qMissing.missingId.count())
+            .where(qMissing.isActive.eq(isActive))
+            .from(qMissing)
+            .fetchOne();
+
+    return new PageImpl<>(results, pageable, total);
+  }
+}
