385a2c37f6ca04002fb715cfc275300006cdc50c
sHu <akgkfk9@gmail.com>
2023-11-09 13:40:05 +0900

    style: 기존에 만들어둔 샘플 코드의 패키지명 변경
    - Test --> sample 명으로 변경

--- src/main/java/com/project/animal/sample/MinioFileStore.java
+++ src/main/java/com/project/animal/sample/MinioFileStore.java
@@ -1,4 +1,4 @@
-package com.project.animal.Test;
+package com.project.animal.sample;
 
 import lombok.RequiredArgsConstructor;
 
--- src/main/java/com/project/animal/sample/MinioTest.java
+++ src/main/java/com/project/animal/sample/MinioTest.java
@@ -1,11 +1,9 @@
-package com.project.animal.Test;
+package com.project.animal.sample;
 
 import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 
