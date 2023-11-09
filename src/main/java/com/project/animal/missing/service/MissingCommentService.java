e334c5fbe635dead3ea801f5547dee0e9892be8c
devsince2021 <dev.ryu2021@gmail.com>
2023-11-09 16:29:53 +0900

    add: MissingCommentNewDto 폴더 위치 변경

--- src/main/java/com/project/animal/missing/service/MissingCommentService.java
+++ src/main/java/com/project/animal/missing/service/MissingCommentService.java
@@ -2,7 +2,7 @@ package com.project.animal.missing.service;
 
 import com.project.animal.missing.domain.MissingComment;
 import com.project.animal.missing.domain.MissingPost;
-import com.project.animal.missing.dto.MissingCommentNewDto;
+import com.project.animal.missing.dto.comment.MissingCommentNewDto;
 import com.project.animal.missing.exception.CommentSaveFailException;
 import com.project.animal.missing.exception.DetailNotFoundException;
 import com.project.animal.missing.repository.MissingCommentRepository;
@@ -10,7 +10,6 @@ import com.project.animal.missing.repository.MissingPostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -27,7 +26,7 @@ public class MissingCommentService {
       Optional<MissingPost> maybePost =  missingPostRepository.findById(dto.getMissingId());
       MissingPost post = maybePost.orElseThrow(() -> new DetailNotFoundException(dto.getMissingId()));
 
-      MissingComment comment = new MissingComment(dto.getMemberId(), post, dto.getComment());
+      MissingComment comment = new MissingComment(dto.getMemberId(), post, dto.getComment(), dto.getParentId());
       MissingComment result = missingCommentRepository.save(comment);
       if (result == null) throw new Exception("no save result");
       return true;
