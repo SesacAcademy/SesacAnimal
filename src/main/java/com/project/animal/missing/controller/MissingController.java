69e7612211667b6447c2de900cf8fd8f234230ef
devsince2021 <dev.ryu2021@gmail.com>
2023-11-21 12:27:12 +0900

    feat: 목록 컨트롤러 이동

--- src/main/java/com/project/animal/missing/controller/MissingController.java
+++ src/main/java/com/project/animal/missing/controller/MissingController.java
@@ -1,17 +1,52 @@
 package com.project.animal.missing.controller;
 
 import com.project.animal.missing.constant.EndPoint;
+import com.project.animal.missing.constant.ViewName;
+import com.project.animal.missing.dto.ListResponseDto;
+import com.project.animal.missing.dto.MissingFilterDto;
+import com.project.animal.missing.dto.MissingListEntryDto;
+import com.project.animal.missing.service.inf.MissingPostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/v1/missing")
 public class MissingController {
 
-  public final int SUCCESS_FLAG = 1;
+  private final MissingPostService missingPostService;
 
-  public final int FAIL_FLAG = 0;
-  public Map<String, String> createLinkConstants(String ...destinations) {
+  @GetMapping("/list")
+  public String getPostList(
+          @ModelAttribute MissingFilterDto filterDto,
+          @PageableDefault(sort="missing_id", direction = Sort.Direction.DESC)
+          Pageable pageable,
+          Model model) {
+
+    ListResponseDto<MissingListEntryDto> result = missingPostService.getPostList(filterDto, pageable);
+    Map<String, String> endPoints = createLinkConstants("detail", "list", "new");
+
+    model.addAttribute("endPoints", endPoints);
+    model.addAttribute("list", result.getList());
+    model.addAttribute("count", result.getTotalCount());
+
+    return ViewName.POST_LIST;
+  }
+
+  private Map<String, String> createLinkConstants(String ...destinations) {
     Map<String, String> endPoints = Map.of(
             "new", EndPoint.MISSING_BASE + EndPoint.NEW,
             "edit", EndPoint.MISSING_BASE + EndPoint.EDIT,
