8ec3622bc3ec0eca0f3ae44bb13c5608cc5a6d58
devsince2021 <dev.ryu2021@gmail.com>
2023-11-22 11:18:15 +0900

    fix: 동시성 이슈 대응

--- src/main/java/com/project/animal/missing/service/MissingLikeCacheServiceImpl.java
+++ src/main/java/com/project/animal/missing/service/MissingLikeCacheServiceImpl.java
@@ -10,55 +10,36 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 
-// TODO: 해당 서비스는 Repository의 Proxy로 사용할지 여부 검토 필요
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MissingLikeCacheServiceImpl implements MissingLikeCacheService {
   private final RedisServiceProvider redisServiceProvider;
 
-  private final MissingLikeRepository missingLikeRepository;
-
   private final String cachePrefix = "missingLike";
   private final int ADD = 1;
 
 
   @Override
-  public int getCountByPostId(long postId) {
+  public Optional<Integer> getCountByPostId(long postId) {
     String likeCountKey = cachePrefix + postId;
-    int currentCount = countByPostId(likeCountKey, postId);
+    Optional<String> maybeCurrentCount = redisServiceProvider.get(likeCountKey);
 
-    return currentCount;
+    Integer currentCount = maybeCurrentCount.isPresent()
+            ? Integer.parseInt(maybeCurrentCount.get())
+            : null;
+
+    return Optional.ofNullable(currentCount);
   }
 
   @Override
   public void updateLike(long postId, int status) {
     String likeCountKey = cachePrefix + postId;
-    int currentCount = countByPostId(likeCountKey, postId);
 
-    int nextCount = status == ADD
-            ? addCount(currentCount)
-            : subCount(currentCount);
-
-    redisServiceProvider.save(likeCountKey, nextCount);
+    if (status == ADD) {
+      redisServiceProvider.increase(likeCountKey);
+    } else {
+      redisServiceProvider.decrease(likeCountKey);
+    }
   }
-
-  private int countByPostId(String key, long postId) {
-    Optional<String> maybeCurrentCount = redisServiceProvider.get(key);
-
-    int currentCount = maybeCurrentCount.isPresent()
-            ? Integer.parseInt(maybeCurrentCount.get())
-            : missingLikeRepository.likedCountByPostId(postId);
-
-    return currentCount;
-  }
-
-  private int addCount(int currentCount) {
-    return currentCount + 1;
-  }
-
-  private int subCount(int currentCount) {
-    return currentCount > 0 ? currentCount - 1 : 0;
-  }
-
 }
