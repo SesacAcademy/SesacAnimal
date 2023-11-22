febdca4f8a36625a031d9cd3a17eb21d7c7d00f2
devsince2021 <dev.ryu2021@gmail.com>
2023-11-22 18:53:59 +0900

    feat: 쿼리 dsl 개선

--- src/main/java/com/project/animal/missing/repository/CustomMissingPostRepositoryImpl.java
+++ src/main/java/com/project/animal/missing/repository/CustomMissingPostRepositoryImpl.java
@@ -1,77 +1,92 @@
 package com.project.animal.missing.repository;
 
 import com.project.animal.missing.domain.MissingPost;
-import com.project.animal.missing.domain.QMissingLike;
 import com.project.animal.missing.domain.QMissingPostImage;
 import com.project.animal.missing.dto.MissingFilterDto;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.util.StringUtils;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import com.project.animal.missing.domain.QMissingPost;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
+@RequiredArgsConstructor
 public class CustomMissingPostRepositoryImpl implements CustomMissingPostRepository {
 
   private final int isActive = 1;
   private final JPAQueryFactory queryFactory;
 
-  @Autowired
-  public CustomMissingPostRepositoryImpl(EntityManager em) {
-    this.queryFactory = new JPAQueryFactory(em);
-  }
+  private final QMissingPost qMissing = QMissingPost.missingPost;
+  private final QMissingPostImage qImage = QMissingPostImage.missingPostImage;
+
 
   @Override
   public Page<MissingPost> findByFilter(MissingFilterDto filter, Pageable pageable) {
-    QMissingPost qMissing = QMissingPost.missingPost;
-    QMissingPostImage qImage = QMissingPostImage.missingPostImage;
-    QMissingLike qLike = QMissingLike.missingLike;
-
-    BooleanBuilder builder = new BooleanBuilder();
-
-    builder.and(qMissing.isActive.eq(isActive));
-
-    if (filter.getAnimalType() != null) {
-      builder.and(qMissing.animalType.equalsIgnoreCase(filter.getAnimalType()));
-    }
-
-    // TODO: fromDate로 조건 검색시 일치하지 않음
-    if (filter.getFromDate() != null) {
-      builder.and(qMissing.missingTime.goe(filter.getFromDate()));
-    }
-
-    if (filter.getEndDate() != null) {
-      builder.and(qMissing.missingTime.loe(filter.getEndDate()));
-    }
-
-    // NOTE: 검색은 타이틀 기준으로 실행
-    if (filter.getSearch() != null && !filter.getSearch().isBlank() && !filter.getSearch().isEmpty()) {
-      builder.and(qMissing.title.containsIgnoreCase(filter.getSearch()));
-    }
 
     List<MissingPost> results = queryFactory
-            .selectFrom(qMissing).distinct()
-            .where(builder)
-            .innerJoin(qMissing.images, qImage)
+            .selectFrom(qMissing)
+            .innerJoin(qMissing.images, qImage).fetchJoin()
+            .where(
+                    eqAnimalType(filter.getAnimalType()),
+                    containKeyword(filter.getSearch()),
+                    goeFromDate(filter.getFromDate()),
+                    loeEndDate(filter.getEndDate()),
+                    eqIsActive(isActive))
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
-            .orderBy()
+            .orderBy(qMissing.updatedAt.desc())
             .fetch();
 
     long total = queryFactory
             .select(qMissing.missingId.count())
-            .where(qMissing.isActive.eq(isActive))
+            .where(
+                    eqAnimalType(filter.getAnimalType()),
+                    containKeyword(filter.getSearch()),
+                    goeFromDate(filter.getFromDate()),
+                    loeEndDate(filter.getEndDate()),
+                    eqIsActive(isActive))
             .from(qMissing)
             .fetchOne();
 
     return new PageImpl<>(results, pageable, total);
   }
+
+  private BooleanExpression eqAnimalType(String animalType) {
+    if (StringUtils.isNullOrEmpty(animalType)) {
+      return null;
+    }
+    return qMissing.animalType.equalsIgnoreCase(animalType);
+  }
+
+  private BooleanExpression containKeyword(String keyword) {
+    if (StringUtils.isNullOrEmpty(keyword)) {
+      return null;
+    }
+    return qMissing.title.containsIgnoreCase(keyword);
+  }
+
+  private BooleanExpression loeEndDate(LocalDateTime endDate) {
+    if (endDate == null) {
+      return null;
+    }
+    return qMissing.missingTime.loe(endDate);
+  }
+
+  private BooleanExpression goeFromDate(LocalDateTime fromDate) {
+    if (fromDate == null) {
+      return null;
+    }
+    return qMissing.missingTime.goe(fromDate);
+  }
+
+  private BooleanExpression eqIsActive(int status) {
+    return qMissing.isActive.eq(status);
+  }
+
 }
