a6fb2954932f0c267ecd9c123b091edd7b339bdc
SON\dhsmf <dhsmf4245@gmail.com>
2023-11-23 21:39:04 +0900

    WIP: 게시글 상세보기 css 작업 필요

--- src/main/java/com/project/animal/review/constant/ViewName.java
+++ src/main/java/com/project/animal/review/constant/ViewName.java
@@ -3,7 +3,7 @@ package com.project.animal.review.constant;
 public class ViewName {
     public static final String REVIEW_LIST = "/review/reviewList";
     public static final String WRITE_PAGE = "/review/writeReview";
-    public static final String READ_ONE = "/review/readReviewFinal";
+    public static final String READ_ONE = "/review/reviewOne";
     public static final String EDIT_ONE = "/review/updateReviewFinal";
     public static final String HOME = "redirect:/review";
     public static final String REVIEW_LIST_BY_SEARCH = "/review/reviewListBySearch";
