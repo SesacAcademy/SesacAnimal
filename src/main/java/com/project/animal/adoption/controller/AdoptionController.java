524348f51687116fdb55e7958ff8b99dfa4e10f3
dev-lkj <2_kj@daum.net>
2023-11-25 13:55:08 +0900

    feat: index breed검색 기능 추가

--- src/main/java/com/project/animal/adoption/controller/AdoptionController.java
+++ src/main/java/com/project/animal/adoption/controller/AdoptionController.java
@@ -1,16 +1,14 @@
 package com.project.animal.adoption.controller;
 
+import com.project.animal.adoption.constant.ViewName;
 import com.project.animal.adoption.domain.Adoption;
 import com.project.animal.adoption.domain.AdoptionComment;
 import com.project.animal.adoption.domain.AdoptionImage;
-import com.project.animal.adoption.domain.AdoptionPostLike;
 import com.project.animal.adoption.dto.*;
 import com.project.animal.adoption.repository.AdoptionRepository;
 import com.project.animal.adoption.service.AdoptionCommentServiceImpl;
 import com.project.animal.adoption.service.AdoptionServiceImpl;
 import com.project.animal.global.common.annotation.Member;
-import com.project.animal.global.common.constant.EndPoint;
-import com.project.animal.global.common.constant.ViewName;
 import com.project.animal.global.common.dto.MemberDto;
 import com.project.animal.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -46,14 +44,11 @@ public class AdoptionController {
 
 
     // 메인 리스트 입장
-    @GetMapping(EndPoint.ADOPTION_LIST)
+    @GetMapping("/v1/adoption")
     public String adoptionMainGet(@RequestParam(required = false) Integer pageNumber,
                                   @RequestParam(required = false) String breed,
                                   Model model,
                                   @Member MemberDto memberDto){
-
-//        String breed = "cat";
-//        System.out.println("breed:>>"+breed);
 
         int pageSize = 10; // 한 페이지에 보여줄 데이터 개수
 
@@ -61,22 +56,18 @@ public class AdoptionController {
             pageNumber = 1; // 페이지 번호가 없거나 1보다 작으면 기본값으로 1 설정
         }
 
-
-//        dogList = adoptionService.getDogList();
-//        for (Adoption adoption : dogList) {
-//            System.out.println("개:>>"+adoption.getBreed());
-//        }
-        //고양이 리스트 가져오기
-//        catList = adoptionService.getCatList();
-        Page<Adoption> breedList = adoptionService.findPatsByBreed(breed, pageNumber, pageSize);
-
-        for (Adoption adoption : breedList) {
-            System.out.println("breadList Check:>>"+adoption.getId());
+        // index페이지에서 넘어오는 강아지, 고양이 리스트 불러오기
+        Page<Adoption> list;
+        if(breed.equals("개") || breed.equals("고양이")){
+            list = adoptionService.findPatsByBreed(breed, pageNumber, pageSize);
+        }else{
+            // 고양이 , 강아지 종으로 넘어오는 경우가 아닌 모든 경우
+            //전체 리스트
+            list = adoptionService.getAdoptionPageWithImagesAndMemberPages(pageNumber,pageSize); // 전체 리스트
         }
-        //전체 리스트
-        Page<Adoption> listWithImagesAndMember = adoptionService.getAdoptionPageWithImagesAndMemberPages(pageNumber,pageSize); // 전체 리스트
 
-        int count = listWithImagesAndMember.getTotalPages(); // 총 불러온 리스트의 갯수
+
+        int count = list.getTotalPages(); // 총 불러온 리스트의 갯수
         int BLOCK_COUNT = 10; // 페이지 넘버 갯수
         int temp = (pageNumber-1)%BLOCK_COUNT; //  페이지 넘버 갯수에 맞게 시작페이지 보이도록 계산해주는 변수
         int startPage = pageNumber-temp; // 페이지네이션에서 보여줄 시작 페이지
@@ -84,14 +75,8 @@ public class AdoptionController {
 //        int pageCount = (count / pageSize) + (count % pageSize == 0 ? 0 : 1); //
 
 
-//        model.addAttribute("list", listWithImagesAndMember);
 
-//        if(breed.equals("dog") || breed.equals("cat")){
-//            model.addAttribute("list", breedList);
-//        }else{
-            model.addAttribute("list", listWithImagesAndMember);
-//        }
-
+        model.addAttribute("list", list);
         model.addAttribute("blockCount", BLOCK_COUNT);
         model.addAttribute("startPage", startPage);
         model.addAttribute("pageNumber", pageNumber);
@@ -102,7 +87,7 @@ public class AdoptionController {
         // 좋아요 리스트를 체크하기 위한 로직
         if(memberDto != null){
             List<Boolean> likeStatusList = new ArrayList<>();
-            for (Adoption adoption : listWithImagesAndMember.getContent()) {
+            for (Adoption adoption : list.getContent()) {
                 boolean liked = adoptionService.isAdoptionLikedByUser(adoption.getId(), memberDto.getId());
                 likeStatusList.add(liked);
             }
@@ -115,7 +100,7 @@ public class AdoptionController {
 
 
     // 입양 게시판 list 좋아요 처리 영역
-    @PostMapping (EndPoint.ADOPTION_LIST)
+    @PostMapping ("/v1/adoption")
     @ResponseBody
     public Map<String, Object> adoptionMainPost(@RequestBody Map<String, String> requestBody, @Member MemberDto memberDto){
 
@@ -140,11 +125,11 @@ public class AdoptionController {
 
 
     // 글 쓰기 들어오기
-    @GetMapping(EndPoint.ADOPTION_WRITE)
+    @GetMapping("/v1/adoption/edit")
     public String adoptionWrite(@Member MemberDto member){
 
         if(member == null ){
-            return "redirect:"+EndPoint.ADOPTION_LIST;
+            return "redirect:/v1/adoption";
         }else{
             return ViewName.ADOPTION_WRITE;
         }
@@ -152,7 +137,7 @@ public class AdoptionController {
 
 
     //  글쓰기 쓰고 post 보내는 영역
-    @PostMapping(EndPoint.ADOPTION_WRITE)
+    @PostMapping("/v1/adoption/edit")
     public String adoptionWritePost(@ModelAttribute @Validated AdoptionWriteDto adoptionWriteDto, BindingResult bindingResult,
                                     @RequestParam(name="image") List<MultipartFile> file,
                                     @Member MemberDto member) {
@@ -160,17 +145,17 @@ public class AdoptionController {
         if(bindingResult.hasErrors()){
             log.info("adoption_write binding error = {}", bindingResult);
 
-            return "redirect:"+EndPoint.ADOPTION_WRITE;
+            return "redirect:/v1/adoption/edit";
         }
 
         adoptionService.save(adoptionWriteDto, file, member);
 
 //        return "redirect:/v1/adoption";
-        return "redirect:"+EndPoint.ADOPTION_LIST;
+        return "redirect:/v1/adoption";
     }
 
     // 글쓰기 영역 adoption_write
-    @GetMapping(EndPoint.ADOPTION_EDIT)
+    @GetMapping("/v1/adoption/edit/{id}")
     public String adoptionEditGet(@PathVariable Long id, Model model){
 
 
@@ -183,7 +168,7 @@ public class AdoptionController {
     }
 
     // 게시글 상세 읽기 영역
-    @GetMapping(EndPoint.ADOPTION_READ)
+    @GetMapping("/v1/adoption/{id}")
     public String adoptionRead(@PathVariable(name="id") Long postId, Model model, @Member MemberDto memberDto){
         // 조회수 올리기
         adoptionService.plusView(postId);
@@ -201,7 +186,7 @@ public class AdoptionController {
 
 
     // 상세 게시글 수정 하고 Post 보내는 영역
-    @PostMapping (EndPoint.ADOPTION_EDIT)
+    @PostMapping ("/v1/adoption/edit/{id}")
     public String adoptionEditPut(@ModelAttribute @Validated AdoptionEditDto adoptionEditDto, BindingResult bindingResult,
                                @RequestParam(name="image") List<MultipartFile> file,
                                   @PathVariable(name="id") Long postId,
@@ -209,7 +194,7 @@ public class AdoptionController {
 
         if(bindingResult.hasErrors()){
             log.info("adoption_edit binding error = {}", bindingResult);
-            return "redirect:"+EndPoint.ADOPTION_EDIT;
+            return "redirect:/v1/adoption/edit/{id}";
 //            return EndPoint.ADOPTION_LIST;
         }
 
@@ -218,14 +203,14 @@ public class AdoptionController {
             adoptionService.update(adoptionEditDto, file, postId);
         }
 
-        return "redirect:"+EndPoint.ADOPTION_LIST;
+        return "redirect:/v1/adoption";
 //        return EndPoint.ADOPTION_LIST;
     }
 
 
     // 게시글 상세 수정 영역 중 게시글, 이미지 삭제 영역
     @CrossOrigin(origins = {"http://localhost:8080", "http://infra.shucloud.site"})
-    @DeleteMapping(EndPoint.ADOPTION_EDIT )
+    @DeleteMapping("/v1/adoption/edit/{id}")
     @ResponseBody
     public ResponseEntity<String> handleImageDeleteRequest(@RequestBody Map<String, String> requestBody, @PathVariable Long id) {
         
@@ -248,7 +233,7 @@ public class AdoptionController {
                 }
                 adoptionService.simpleSave(adoption);
 
-                return ResponseEntity.ok().body(EndPoint.ADOPTION_LIST);
+                return ResponseEntity.ok().body("/v1/adoption");
             }else{
                 log.info("입양 게시판 이미지 및 게시글 삭제 둘다 안된 경우");
             }
@@ -265,7 +250,7 @@ public class AdoptionController {
 
 
     // 댓글 쓰기 (읽기영역 내)
-    @PostMapping(EndPoint.ADOPTION_READ)
+    @PostMapping("/v1/adoption/{id}")
     public String adoptionCommentPost(@ModelAttribute @Validated AdoptionCommentWriteDto adoptionCommentWriteDto, BindingResult bindingResult,
                                       @PathVariable(name = "id") Long postId,
                                       @RequestParam(name="commentId", required = false) Long commentId,
@@ -274,7 +259,7 @@ public class AdoptionController {
         if(bindingResult.hasErrors()){
             log.info("adoption_read 영역 내 댓글 에러 ={}", bindingResult);
 
-            return "redirect:"+EndPoint.ADOPTION_READ;
+            return "redirect:/v1/adoption/{id}";
         }
 
         if (commentId != null) {
@@ -285,12 +270,12 @@ public class AdoptionController {
             adoptionCommentService.saveComment(adoptionCommentWriteDto, postId, memberDto);
         }
 
-        return "redirect:"+EndPoint.ADOPTION_READ;
+        return "redirect:/v1/adoption/{id}";
     }
 
     // 댓글 / 대댓글 삭제
     @CrossOrigin(origins = {"http://localhost:8080", "http://infra.shucloud.site"})
-    @DeleteMapping(EndPoint.ADOPTION_READ)
+    @DeleteMapping("/v1/adoption/{id}")
     @ResponseBody
     public ResponseEntity<String> adoptionCommentDelete(@RequestBody Map<String, String> requestBody, @PathVariable(name="id") Long postId){
 
@@ -307,7 +292,7 @@ public class AdoptionController {
                 // 대댓글만 삭제 로직 수행
                 adoptionCommentService.deleteChildComment(deleteCommentChildIndex);
             }
-            return ResponseEntity.ok(EndPoint.ADOPTION_READ);
+            return ResponseEntity.ok("/v1/adoption/{id}");
         } catch (Exception e) {
             log.error("댓글 삭제 중 오류 발생: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("fail Comment delete");
--- src/main/java/com/project/animal/global/common/constant/ViewName.java
+++ src/main/java/com/project/animal/global/common/constant/ViewName.java
@@ -18,10 +18,4 @@ public abstract class ViewName {
     public static final String MYPAGE_ADOPTION_LIST = MEMBER_PREFIX + "/mypage_adoptionList";
 
 
-    public static final String ADOPTION_PREFIX = "adoption";
-    public static final String ADOPTION_LIST = ADOPTION_PREFIX +"/adoption_list"; // 입양 게시판 메인 리스트
-    public static final String ADOPTION_WRITE = ADOPTION_PREFIX + "/adoption_write";
-    public static final String ADOPTION_EDIT = ADOPTION_PREFIX + "/adoption_edit";
-    public static final String ADOPTION_READ = ADOPTION_PREFIX + "/adoption_read";
-
 }
